d271287b907f68706f3aa21c4293a218a25dab95
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2020-01-29 14:43:48 +0200

    Add layout for pizza details

--- app/src/main/java/com/vaios/jalapeno/list/BBQFragment.kt
+++ app/src/main/java/com/vaios/jalapeno/list/BBQFragment.kt
@@ -1,5 +1,16 @@
 package com.vaios.jalapeno.list
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.vaios.jalapeno.R
+import kotlinx.android.synthetic.main.fragment_pizza_details.*
 
-class BBQFragment : Fragment()
+class BBQFragment : Fragment(R.layout.fragment_pizza_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_pizza_name.setText(R.string.pizza_bbq)
+    }
+
+}
--- app/src/main/java/com/vaios/jalapeno/list/DiavolaFragment.kt
+++ app/src/main/java/com/vaios/jalapeno/list/DiavolaFragment.kt
@@ -1,5 +1,17 @@
 package com.vaios.jalapeno.list
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.vaios.jalapeno.R
+import kotlinx.android.synthetic.main.fragment_pizza_details.*
 
-class DiavolaFragment : Fragment()
+class DiavolaFragment : Fragment(R.layout.fragment_pizza_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_pizza_name.setText(R.string.pizza_diavola)
+    }
+
+}
+

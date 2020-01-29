eafd05f2468f956687d92634532cc2fc1f87e774
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2020-01-29 14:39:07 +0200

    Add layout for list pizzas

--- app/src/main/java/com/vaios/jalapeno/list/PizzaListFragment.kt
+++ app/src/main/java/com/vaios/jalapeno/list/PizzaListFragment.kt
@@ -1,5 +1,6 @@
 package com.vaios.jalapeno.list
 
 import androidx.fragment.app.Fragment
+import com.vaios.jalapeno.R
 
-class PizzaListFragment : Fragment()
+class PizzaListFragment : Fragment(R.layout.fragment_pizzas_list)
